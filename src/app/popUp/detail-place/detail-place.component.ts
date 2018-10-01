2960a9f590f13674701c7000139b277cb530f2ec
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-10-01 13:27:59 -0300

    component

---
+++ src/app/popUp/detail-place/detail-place.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-place',
+  templateUrl: './detail-place.component.html',
+  styleUrls: ['./detail-place.component.css']
+})
+export class DetailPlaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
