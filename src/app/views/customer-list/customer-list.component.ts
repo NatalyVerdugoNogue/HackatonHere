d0620e1e21d50db124831cd74800e626a47484b8
Natalia <nataliamh.88@gmail.com>
2018-10-02 21:08:04 -0300

    eliminar y filtrar evento

--- src/app/views/customer-list/customer-list.component.ts
+++ src/app/views/customer-list/customer-list.component.ts
@@ -7,12 +7,35 @@ import { CustomerService } from '../../services/customer.service';
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css']
 })
+
 export class CustomerListComponent implements OnInit {
 
   constructor(private customerService: CustomerService) { }
+  eventosArray = [];
+  showDeletedMessage: boolean;
+  searchText: string = "";
 
   ngOnInit() {
-    this.customerService.getListOfEvents();
+    this.customerService.getListOfEvents().subscribe(list => {
+      this.eventosArray = list.map(item => {
+        return {
+          $key: item.key,
+          ...item.payload.val()
+          // me devolvera el objeto con solo los valores
+          // convertimos el objeto en array
+        }
+      })
+    });
+  }
+  onDelete($key) {
+    if (confirm('Confirmar para eliminar evento')) {
+      this.customerService.deleteEvent($key);
+      this.showDeletedMessage = true;
+      setTimeout(() => this.showDeletedMessage = false, 3000);
+    }
   }
 
+  filterCondition(evento) {
+    return evento.nombreEvento.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1;
+  }
 }
