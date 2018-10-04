330e811e17f708adfdca610f990fae7bbbdc8118
Natalia <nataliamh.88@gmail.com>
2018-10-04 10:08:18 -0300

    navbar

--- src/app/views/navbar/navbar.component.ts
+++ src/app/views/navbar/navbar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +14,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  options: FormGroup;
+  opened = true;
+  over = 'side';
+  expandHeight = '100vh';
+  collapseHeight = '10vh';
+  displayMode = 'flat';
+  watcher: Subscription;
+
+  userName: any;
+  photo: any;
+  email: any;
+
+  // tslint:disable-next-line:max-line-length
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
 
+/*   onLogout() {
+    this.authService.logout()
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch(() => {
+        this.snackBar.open('Error al tratar de cerrar sesión, trata otra vez'
+          , null
+          , {
+            duration: 3000
+          });
+      });
+  } */
 }
