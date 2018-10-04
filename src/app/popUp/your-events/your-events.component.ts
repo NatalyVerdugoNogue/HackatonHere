237f91661bddf2a2e19d5005d3694d341a4fa0d7
Natalia <nataliamh.88@gmail.com>
2018-10-03 22:50:35 -0300

    cambie a your events component

--- src/app/popUp/your-events/your-events.component.ts
+++ src/app/popUp/your-events/your-events.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event.service';
 
 @Component({
   selector: 'app-your-events',
   templateUrl: './your-events.component.html',
   styleUrls: ['./your-events.component.css']
 })
+
 export class YourEventsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
+  eventosArray = [];
 
   ngOnInit() {
+    this.eventService.getListOfEvents().subscribe(list => {
+      this.eventosArray = list.map(item => {
+        return {
+          $key: item.key,
+          ...item.payload.val()
+          // me devolvera el objeto con solo los valores
+          // convertimos el objeto en array
+        }
+      })
+    });
   }
-
 }
